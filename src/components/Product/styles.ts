967b54f5fdf0c3a9325fb60c1c01c597e97f6cdd
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2024-10-23 21:33:32 -0300

    Integração Api - Parte 2: fazendo a integração parte interna Página | Parametrizando as Propriedade necessárias

--- src/components/Product/styles.ts
+++ src/components/Product/styles.ts
@@ -1,12 +1,16 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
 import { TagContainer } from '../Tag/styles'
+import { Link } from 'react-router-dom'
 
-export const Card = styled.div`
+export const Card = styled(Link)`
   background-color: ${cores.cinza};
   padding: 8px;
   border-radius: 8px;
   position: relative;
+  text-decoration: none;
+  color: ${cores.branca};
+  display: block;
 
   img {
     display: block;
@@ -17,7 +21,6 @@ export const Card = styled.div`
 
   ${TagContainer} {
     margin-right: 8px;
-    padding-left
   }
 `
 
